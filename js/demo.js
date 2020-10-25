b13e9a1b25297cfb4bdfe8fcc559e3d861e310f5
withus0147 <73203866+withus0147@users.noreply.github.com>
2020-10-25 16:54:44 +0900

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,27 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#e67401',
+    lineColor: '#e67401'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 1 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
